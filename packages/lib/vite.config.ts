b57283d5e2ebe23f7b8a8de0e4cb6d9d626473b3
Rasmus Gustafsson <rasmus.gustafsson2611@gmail.com>
2024-02-27 03:58:14 +0200

    chore: setup turborepo

--- packages/lib/vite.config.ts
+++ packages/lib/vite.config.ts
@@ -5,6 +5,7 @@ import { defineConfig } from "vite"
 export default defineConfig({
   test: {
     coverage: {
+      // @ts-ignore
       "100": true,
       include: ["src"],
       reporter: ["text", "json", "html"],
